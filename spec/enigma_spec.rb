3cdd0116caccea99276d0d42f27830aedcf70c31
Matt Kragen <mattdkragen@gmail.com>
2021-06-12 14:20:55 -0700

    Start Enigma spec with existence test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,1 +1,11 @@
 require_relative './spec_helper'
+require 'date'
+
+RSpec.describe Enigma do
+  context 'instantiation' do
+    it 'exists' do
+      enigma = Enigma.new
+      expect(enigma).to be_a(Enigma)
+    end
+  end
+end
