ba7bf1951d392b7066e4ab56487afa30963cd28f
Matt Kragen <mattdkragen@gmail.com>
2021-06-12 14:31:37 -0700

    Add encryption and decryption tests for Enigma

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -8,4 +8,26 @@ RSpec.describe Enigma do
       expect(enigma).to be_a(Enigma)
     end
   end
+
+  context 'methods' do
+    xit 'encrypts messages'
+      enigma = Enigma.new
+      encryption = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+      expect(enigma.encrypt("hello world", "02715", "040895")).to eq(encryption)
+    end
+
+    xit 'decrypts messages' do
+      enigma = Enigma.new
+      decryption = {
+        decryption: "hello world",
+        key: "02715",
+        date: "040895"
+      }
+      expect(enigma.decrypt("keder ohulw", "02715", "040895"))
+    end
+  end
 end
