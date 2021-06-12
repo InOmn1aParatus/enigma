1ddf57b755810ac0c70c5def79d405b1ea14ce3d
Matt Kragen <mattdkragen@gmail.com>
2021-06-12 15:03:52 -0700

    Add tests for encryption and decryption

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -20,6 +20,16 @@ RSpec.describe Enigma do
       expect(enigma.encrypt("hello world", "02715", "040895")).to eq(encryption)
     end
 
+    xit 'encrypts using key only' do
+      enigma = Enigma.new
+      encryption = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: # today's date in format "DDMMYY"
+      }
+      expect(enigma.encrypt("hello world", "02715")).to eq(encryption)
+    end
+
     xit 'decrypts messages' do
       enigma = Enigma.new
       decryption = {
@@ -28,6 +38,26 @@ RSpec.describe Enigma do
         date: "040895"
       }
       expect(enigma.decrypt("keder ohulw", "02715", "040895"))
+    end
+
+    xit 'decrypts using key only' do
+      enigma - Enigma.new
+      decryption = {
+        decryption: "hello world",
+        key: "02715",
+        date: # today's date in format "DDMMYY"
+      }
+      expect(enigma.decrypt(encrypted[:encryption], "02715")).to eq(decryption)
+    end
+
+    xit 'encrypts a message using random key + current date' do
+      enigma = Enigma.new
+      encryption = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: # today's date in format "DDMMYY"
+      }
+      expect(enigma.encrypt('hello world')).to eq(encryption)
     end
   end
 end
