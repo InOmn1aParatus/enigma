9d69b4e06ee8b5616be46bc63d95e70a41298aaf
Matt Kragen <mattdkragen@gmail.com>
2021-06-13 12:56:45 -0700

    Create existence and #gen test

--- spec/key_gen_spec.rb
+++ spec/key_gen_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe KeyGen do
       expect(key_gen).to be_a(KeyGen)
     end
   end
+
+  context 'methods' do
+    it 'generates random key' do
+      key_gen = KeyGen.new
+      expect(key_gen.gen).to be_a(String)
+      expect(key_gen.gen.length).to eq(5)
+    end
+  end
 end
