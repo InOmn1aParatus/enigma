f7b631f1dc79f2b743b83a7650e85d40cf70a72e
Matt Kragen <mattdkragen@gmail.com>
2021-06-13 16:31:35 -0700

    Add requirements; insert object instances into #init

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,8 @@
 require_relative './character_list'
 require_relative './encrypt'
 require_relative './decrypt'
+require_relative './key_gen'
+require_relative './offset'
 
 class Enigma
   attr_reader :characters
@@ -9,5 +11,7 @@ class Enigma
     @characters = CharacterList.new.list
     # @encrypt = Encrypt.new.encrypt
     # @encrypt = Decrypt.new.decrypt
+    # @key = KeyGen.new
+    # @offset = Offset.new
   end
 end
