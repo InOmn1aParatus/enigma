b84bbb2e8193bd6274cc89684c102bd251d90e11
Matt Kragen <mattdkragen@gmail.com>
2021-06-13 10:59:15 -0700

    Create existence + attribute test for char list

---
+++ spec/character_list_spec.rb
@@ -0,0 +1,16 @@
+require_relative './spec_helper'
+
+RSpec.describe CharacterList do
+  context 'instantiation' do
+    it 'exists' do
+      char_list = CharacterList.new
+      expect(char_list).to be_a(CharacterList)
+    end
+
+    it 'has attributes' do
+      char_list = CharacterList.new
+      expect(char_list.list[0]).to eq('a')
+      expect(char_list.list[26]).to eq(' ')
+    end
+  end
+end
