fc88b0bb1a0aab53da7c31c5bd540cb962418907
Matt Kragen <mattdkragen@gmail.com>
2021-06-13 12:24:58 -0700

    Add encrypt and decrypt instances to #init; comment out

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,16 @@
-class Enigma
+require_relative './character_list'
+require_relative './encrypt'
+require_relative './decrypt'
 
-  def encrypt(message, key = random_key, date = Date.today)
+class Enigma
+  attr_reader :characters
+
+  def initialize
+    @characters = CharacterList.new.list
+    # @encrypt = Encrypt.new.encrypt
+    # @encrypt = Decrypt.new.decrypt
   end
 
-  
+  # def encrypt(message, key = random_key, date = Date.today)
+  # end
 end
