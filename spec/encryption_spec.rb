96e9fff73e10bf9bd72085b714dcae50d178375a
Matt Kragen <mattdkragen@gmail.com>
2021-06-14 16:42:23 -0700

    Add test for character attribute

--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -3,14 +3,27 @@ require_relative './spec_helper'
 RSpec.describe Encryption do
   context 'instantiation' do
     it 'exists' do
-      e = Encryption.new
-      expect(e).to be_a(Encryption)
+      en = Encryption.new
+      expect(en).to be_a(Encryption)
+    end
+
+    it 'hosts character list' do
+      en = Encryption.new
+      expect(en.characters[0]).to eq('a')
+      expect(en.characters[26]).to eq(' ')
     end
   end
 
   context 'methods' do
-    # it 'performs decryption' do
 
+    # xit 'encrypts messages'
+    #   en = Encryption.new
+    #   encryption = {
+    #     encryption: "keder ohulw",
+    #     key: "02715",
+    #     date: "040895"
+    #   }
+    #   expect(en.encrypt("hello world", "02715", "040895")).to eq(encryption)
     # end
   end
 end
