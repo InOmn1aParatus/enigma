8e2daa72479ff323b73d617ede6de6e16767f325
Matt Kragen <mattdkragen@gmail.com>
2021-06-14 13:20:41 -0700

    Update CharList with attr reader; change method test to attribute test

--- lib/character_list.rb
+++ lib/character_list.rb
@@ -1,4 +1,5 @@
 class CharacterList
+  attr_reader :list
 
   def initialize
     @list = ('a'..'z').to_a << ' '
--- spec/character_list_spec.rb
+++ spec/character_list_spec.rb
@@ -6,13 +6,11 @@ RSpec.describe CharacterList do
       char_list = CharacterList.new
       expect(char_list).to be_a(CharacterList)
     end
+
+    it 'contains a list of characters' do
+      char_list = CharacterList.new
+      expect(char_list.list[0]).to eq('a')
+      expect(char_list.list[26]).to eq(' ')
+    end
   end
-end
-  context 'methods' do
-  #   it 'contains list of characters' do
-  #     char_list = CharacterList.new
-  #     expect(char_list.list[0]).to eq('a')
-  #     expect(char_list.list[26]).to eq(' ')
-  #   end
-  # end
 end
