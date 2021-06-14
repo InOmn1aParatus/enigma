83a1c749e35db29c2ee659ef24410513b9fbfa7b
Matt Kragen <mattdkragen@gmail.com>
2021-06-13 18:20:01 -0700

    Add test for offset generation

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Offset do
       expect(offset).to be_a(Offset)
     end
   end
+
+  context ' methods' do
+    it 'generates offset based on given date' do
+      offset = Offset.new
+      expect(offset.generate('200989')).to eq('8121')
+    end
+  end
 end
