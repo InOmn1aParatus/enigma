c04a2cdb9727cb2e2f28fdf00a4841ad60df7ed4
Matt Kragen <mattdkragen@gmail.com>
2021-06-13 20:17:56 -0700

    Update #generate; move #key_gen into Offset

--- lib/offset.rb
+++ lib/offset.rb
@@ -2,8 +2,8 @@ require 'date'
 
 class Offset
   def generate(date = Date.today.strftime('%d%m%y'))
-    squared = date.to_i**2
-    offset = squared.to_s[-4..-1]
+    date_squared = date.to_i**2
+    offset = date_squared.to_s[-4..-1]
   end
 
   def key_gen
