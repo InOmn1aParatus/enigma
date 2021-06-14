09477e6685b8d648eedcd166ab53f30ee6a139bd
Matt Kragen <mattdkragen@gmail.com>
2021-06-14 10:52:54 -0700

    Finish offset/key-gen methods

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,9 +1,26 @@
 require 'date'
 
 class Offset
-  def generate(date = Date.today.strftime('%d%m%y'))
-    date_squared = date.to_i**2
-    offset = date_squared.to_s[-4..-1]
+  attr_reader :key, :date, :offset
+
+  def initialize(key: nil, date: nil)
+    if key.nil?
+      @key = key_gen
+    else
+      @key = key
+    end
+  
+    if date.nil?
+      @date = Date.today.strftime('%d%m%y')
+    else
+      @date = date
+    end
+    offset_gen
+  end
+
+  def offset_gen
+    squared = @date.to_i**2
+    @offset = squared.to_s[-4..-1]
   end
 
   def key_gen
