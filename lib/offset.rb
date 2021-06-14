12cd9880e956215e6e9ccd79f30f3876853f6554
Matt Kragen <mattdkragen@gmail.com>
2021-06-14 12:18:26 -0700

    Create methods for A-D rotations

--- lib/offset.rb
+++ lib/offset.rb
@@ -3,7 +3,7 @@ require 'date'
 class Offset
   attr_reader :key, :date, :offset
 
-  def initialize(key: nil, date: nil)
+  def initialize(key = nil, date = nil)
     if key.nil?
       @key = key_gen
     else
@@ -26,4 +26,21 @@ class Offset
   def key_gen
     rand(99_999).to_s.rjust(5, '0')
   end
+
+  def a_rotation
+    @key[0..1]
+  end
+
+  def b_rotation
+    @key[1..2]
+  end
+
+  def c_rotation
+    @key[2..3]
+  end
+
+  def d_rotation
+    @key[3..4]
+  end
+
 end
