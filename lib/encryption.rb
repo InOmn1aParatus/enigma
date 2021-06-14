7bb9ce336d9250370ceb972d27c87bdba0b3e710
Matt Kragen <mattdkragen@gmail.com>
2021-06-14 16:43:22 -0700

    Put CharacterList into #init

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,2 +1,9 @@
+require_relative './character_list'
+require_relative './offset'
 class Encryption
+  attr_reader :characters
+
+  def initialize
+    @characters = CharacterList.new.list
+  end
 end
