b58bc8783d26c2b6ff7627d1f383999711877f6c
Matt Kragen <mattdkragen@gmail.com>
2021-06-14 10:52:27 -0700

    Fix tests for attributes and offset generation

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -6,23 +6,38 @@ RSpec.describe Offset do
       offset = Offset.new
       expect(offset).to be_a(Offset)
     end
+
+    it 'generates key and date attributes when none are given' do
+      allow_any_instance_of(Offset).to receive(:key_gen).and_return('12345')
+      allow(Date).to receive(:today).and_return(Date.new(2021, 06, 14))
+      offset = Offset.new
+      expect(offset.key).to eq('12345')
+      expect(offset.offset).to eq('5641')
+    end
   end
 
-  context ' methods' do
+  context 'methods' do
     it 'generates random key' do
       offset = Offset.new
       expect(offset.key_gen.length).to eq(5)
     end
     
     it 'generates offset based on given date' do
-      offset = Offset.new
-      expect(offset.generate('200989')).to eq('8121')
+      offset = Offset.new(date: '200989')
+      expect(offset.offset).to eq('8121')
     end
 
     it 'generates offset with current date' do
       offset = Offset.new
-      expect(offset.generate).to be_a(String)
-      expect(offset.generate.length).to eq(4)
+      expect(offset.offset_gen).to be_a(String)
+      expect(offset.offset_gen.length).to eq(4)
+    end
+
+    xit 'uses key to create A-D shift' do
+      offset = Offset.new
+      allow(offset).to receive(:key_gen).and_return('12345')
+      offset
+
     end
   end
 end
