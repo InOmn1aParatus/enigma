15f41a0a77b940a1c232a5cd02177ffa0b876c1d
Matt Kragen <mattdkragen@gmail.com>
2021-06-15 06:11:07 -0700

    Add path to offset in en/de-crypt methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,15 +4,15 @@ require_relative './decryption'
 class Enigma
   attr_reader :encryption, :decryption
 
-  def encrypt(message, key = nil, date = Date.today.strftime('%d%m%y'))
+  def encrypt(message, key = nil, date = nil)
     @encryption = Encryption.new(key, date)
     result = @encryption.encrypt(message)
-    Hash[encryption: result, key: @encryption.key, date: date]
+    Hash[encryption: result, key: @encryption.offset.key, date: @encryption.offset.date]
   end
 
-  def decrypt(message, key = nil, date = Date.today.strftime('%d%m%y'))
+  def decrypt(message, key = nil, date = nil)
     @decryption = Decryption.new(key, date)
     result = @decryption.decrypt(message)
-    Hash[decryption: result, key: @decryption.key, date: date]
+    Hash[decryption: result, key: @decryption.offset.key, date: @decryption.offset.date]
   end
 end
