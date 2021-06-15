a7827098bba420badbda44c773db88a304d57d4e
Matt Kragen <mattdkragen@gmail.com>
2021-06-15 00:31:24 -0700

    ECreate attr reader variables to track dates

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,14 +2,17 @@ require_relative './encryption'
 require_relative './decryption'
 
 class Enigma
+  attr_reader :encryption, :decryption
 
   def encrypt(message, key = nil, date = Date.today.strftime('%d%m%y'))
-    result = Encryption.new(key, date).encrypt(message)
+    @encryption = Encryption.new(key, date)
+    result = @encryption.encrypt(message)
     Hash[ encryption: result, key: key, date: date]
   end
 
   def decrypt(message, key = nil, date = Date.today.strftime('%d%m%y'))
-    result = Decryption.new(key, date).decrypt(message)
+    @decryption = Decryption.new(key, date)
+    result = @decryption.decrypt(message)
     Hash[ decryption: result, key: key, date: date]
   end
   
