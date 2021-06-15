61188cb15f2d41aaf995c7bee02f51ab37311621
Matt Kragen <mattdkragen@gmail.com>
2021-06-15 05:59:14 -0700

    Add instance variables to methods to acquire date in hash

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,12 +7,12 @@ class Enigma
   def encrypt(message, key = nil, date = Date.today.strftime('%d%m%y'))
     @encryption = Encryption.new(key, date)
     result = @encryption.encrypt(message)
-    Hash[encryption: result, key: key, date: date]
+    Hash[encryption: result, key: @encryption.key, date: date]
   end
 
   def decrypt(message, key = nil, date = Date.today.strftime('%d%m%y'))
     @decryption = Decryption.new(key, date)
     result = @decryption.decrypt(message)
-    Hash[decryption: result, key: key, date: date]
+    Hash[decryption: result, key: @decryption.key, date: date]
   end
 end
