c3f4b70c995612015ba15993cd6ea748883bed30
Matt Kragen <mattdkragen@gmail.com>
2021-06-14 23:00:06 -0700

    Update encrypt + decrypt methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,12 +3,12 @@ require_relative './decryption'
 
 class Enigma
 
-  def encrypt(message, key = nil, date = nil)
+  def encrypt(message, key = nil, date = Date.today.strftime('%d%m%y'))
     result = Encryption.new(key, date).encrypt(message)
     Hash[ encryption: result, key: key, date: date]
   end
 
-  def decrypt(message, key = nil, date = nil)
+  def decrypt(message, key = nil, date = Date.today.strftime('%d%m%y'))
     result = Decryption.new(key, date).decrypt(message)
     Hash[ decryption: result, key: key, date: date]
   end
