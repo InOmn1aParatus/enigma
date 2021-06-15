6877285a8ccdb780129f3d54b83dd78382d79ac9
Matt Kragen <mattdkragen@gmail.com>
2021-06-14 19:33:10 -0700

    Correct for updates

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,13 +4,8 @@ require_relative './decryption'
 require_relative './offset'
 
 class Enigma
-  attr_reader :characters
 
   def initialize
-    @characters = CharacterList.new.list
-    # @encrypt = Encryption.new.encrypt
-    # @encrypt = Decryption.new.decrypt
-    # @key = KeyGen.new
-    # @offset = Offset.new
   end
+  
 end
